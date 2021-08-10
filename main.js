5cac22b40faf30f0155acddab4601553370f7103
Juan Chapur <juanchapur99@gmail.com>
2021-08-09 21:49:34 -0300

    Modularizacion y Validacion

--- main.js
+++ main.js
@@ -1,61 +1,120 @@
-document.getElementById('formTask').addEventListener('submit', saveTask);
-
-function saveTask(e) {
-	e.preventDefault();
-	let title = document.getElementById('title').value;
-	let description = document.getElementById('description').value;
-
-	const task = {
-		title,
-		description
-	};
-
-	if (localStorage.getItem('tasks') === null) {
-		let tasks = [];
-		tasks.push(task);
-		localStorage.setItem('tasks', JSON.stringify(tasks));
-	} else {
-		let tasks = JSON.parse(localStorage.getItem('tasks'));
-		tasks.push(task);
-		localStorage.setItem('tasks', JSON.stringify(tasks));
-	}
-	getTasks();
-	document.getElementById('formTask').reset();
-	e.preventDefault();
+class Task {
+    constructor(title, description) {
+        this.title = title;
+        this.description = description;
+    }
 }
 
-function deleteTask(title) {
-	let tasks = JSON.parse(localStorage.getItem('tasks'));
-	for (let i = 0; i < tasks.length; i++) {
-		if (tasks[i].title == title) {
-			tasks.splice(i, 1);
-		}
-	}
-	localStorage.setItem('tasks', JSON.stringify(tasks));
-	getTasks();
+document.getElementById('formTask').addEventListener('submit', setTask);
+
+function createElementWithContent(element, content) {
+    const node = document.createElement(element);
+    node.textContent = content ?? "";
+    return node;
 }
 
+function createElementWithAttributes(element, attributes) {
+    const node = document.createElement(element);
+    for (const key in attributes) {
+        node.setAttribute(key, attributes[key]);
+    }
+    return node;
+}
 
+function createElementComplete(element, attributes, content) {
+    const node = document.createElement(element);
+    for (const key in attributes) {
+        node.setAttribute(key, attributes[key]);
+    }
+    node.textContent = content ?? "";
+    return node;
+}
+
+function isEmpty(task) {
+    let isEmpty = false
+    Object.entries(task).forEach(item => {
+        if (item[1] == "" || item[1] == undefined || item[1] == null) {
+            isEmpty = true;
+        }
+    })
+    return isEmpty;
+}
+
+isEmpty({a:"", b:""})
+
+function taskExist(task) {
+    return getTasks().find(currentTask => currentTask.title == task.title);
+}
+
+function setTask(e) {
+    e.preventDefault();
+    const form = new FormData(e.target);
+    const task = new Task(form.get("title"), form.get("description"));
+    if (!isEmpty(task)) {
+        if (!taskExist(task)) {
+            const tasks = getTasks();
+            tasks.push(task);
+            localStorage.setItem("tasks", JSON.stringify(tasks));
+            displayTasks();
+            document.getElementById('formTask').reset();
+        } else {
+            displayError("The task already exists")
+        }
+    } else {
+        displayError("Field is empty");
+    }
+}
+
+function deleteTask(titleTask) {
+    let tasks = JSON.parse(localStorage.getItem('tasks'));
+    tasks.forEach(item => {
+        if (item.title == titleTask) {
+            tasks.splice(item, 1);
+        }
+    });
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    displayTasks();
+}
+
+function createTaskCard({ title, description }) {
+    const row = createElementWithAttributes("div", { class: "row mx-1" });
+    const card = createElementWithAttributes("div", { class: "card mb-2 bg-dark text-light" });
+    const cardBody = createElementWithAttributes("div", { class: "card-body" });
+    const cardTitle = createElementWithAttributes("div", { class: "card-title" });
+    const h5title = createElementWithContent("h5", title);
+    const cardText = createElementComplete("p", { class: "card-text" }, description);
+    const btnDelete = createElementComplete("a", { id: title, class: "btn btn-danger float-end" }, "Delete");
+    row.appendChild(card);
+    card.appendChild(cardBody);
+    cardBody.appendChild(cardTitle);
+    cardTitle.appendChild(h5title);
+    cardBody.appendChild(cardText);
+    btnDelete.addEventListener("click", function (e) {
+        deleteTask(e.target.id);
+    });
+    cardBody.appendChild(btnDelete);
+    return row;
+}
+
+function displayError(mensaje) {
+    const alert = createElementComplete("div", { class: "alert alert-danger m-3" }, mensaje);
+    document.getElementById("menuAdd").appendChild(alert);
+    setTimeout(function () {
+        document.querySelector('.alert').remove();
+    }, 3000);
+}
+
+function displayTasks() {
+    document.getElementById("tasks").textContent = "";
+    getTasks().forEach(task => {
+        document.getElementById("tasks").appendChild(createTaskCard(task));
+    });
+}
 
 function getTasks() {
-	let tasks = JSON.parse(localStorage.getItem('tasks'));
-	let tasksView = document.getElementById('tasks');
-	tasksView.innerHTML = '';
-	for (let i = 0; i < tasks.length; i++) {
-		let title = tasks[i].title;
-		let description = tasks[i].description;
-
-		tasksView.innerHTML += `
-		<div class="row mx-1">
-	  		<div class="card mb-2 bg-dark text-light">
-				<div class="card-body">
-                	<div class="card-title"><h5>${title}</h5></div>
-    				<p class="card-text">${description}</p>
-					<a href="#" onclick="deleteTask('${title}')" class="btn btn-danger float-end">Delete</a>
-				</div>
-			</div>
-		</div>`;
-	}
+    const data = JSON.parse(localStorage.getItem('tasks'));
+    console.log(data);
+    return data ?? [];
 }
 
-getTasks();
+displayTasks();
